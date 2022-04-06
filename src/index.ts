22a5c42f457498076e1795c7f771fc7654d12dc3
Mikael Holm <mikael.holm@mail.be>
2022-04-06 18:30:13 +0300

    Finish login system

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { config } from 'dotenv'
 config()
+import admin from 'firebase-admin'
+admin.initializeApp({
+  credential: admin.credential.cert('./secrets/firebase.json'),
+})
 import app from './app'
 
 app.listen(8080)
